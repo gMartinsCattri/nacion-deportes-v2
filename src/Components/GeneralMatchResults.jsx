21a80114905668a18406451e675130c2687f964e
guille-m369 <guille.ma369@gmail.com>
2023-03-23 20:06:25 -0300

    partidos generales

--- src/Components/GeneralMatchResults.jsx
+++ src/Components/GeneralMatchResults.jsx
@@ -1,27 +1,75 @@
 import React, {useState, useEffect} from 'react'
 import style from './GeneralMatchResults.module.css'
 import axios from 'axios'
-
 function GeneralMatchResults() {
-    const [matchResults, setMatchResults] = useState([]);
+  const [sportsData, setSportsData] = useState([]);
+  const [soccerResults, setSoccerResults] = useState([]);
+  const [basketballResults, setBasketballResults] = useState([]);
 
-    useEffect(() => {
-        const fetchResults = async () => {
-          try {
-            const matchResponse = await axios.get('https://alfred.to/reservas/sports-nation/v2/main-page', { headers: { Authorization: ' Basic QWxmcmVkOlREODI0MThZYlBweCpuWDV4WDNrSlRrVFNeRTZndQ==' } });
-            setMatchResults(matchResponse.data);
-            
-          } catch (error) {
-            console.log(error);
-          }
-        };
-        fetchResults();
-      }, []);
 
-      console.log(matchResults)
+  useEffect(() => {
+    const fetchResults = async () => {
+      try {
+        const matchResponse = await axios.get('https://alfred.to/reservas/sports-nation/v2/main-page', { headers: { Authorization: ' Basic QWxmcmVkOlREODI0MThZYlBweCpuWDV4WDNrSlRrVFNeRTZndQ==' } });
+        setSportsData(matchResponse.data);
+        
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchResults();
+  }, []);
+
+
+  useEffect(() => {
+    // após receber os dados, filtramos as informações que nos interessam
+    const soccerLeague = sportsData.find(
+      (sport) => sport.sportName === "soccer"
+    );
+    if (soccerLeague) {
+      const soccerLeagueResults = soccerLeague.sportLeagues[0].sportResults;
+      setSoccerResults(soccerLeagueResults);
+    }
+
+    const basketballLeague = sportsData.find(
+      (sport) => sport.sportName === "baloncesto"
+    );
+    if (basketballLeague) {
+      const basketballLeagueResults = basketballLeague.sportLeagues[0].sportResults;
+      setBasketballResults(basketballLeagueResults);
+    }
+  }, [sportsData]);
   return (
     <div className={style.ContainerGeneralMatchResults}>
 
+    <div className={style.GeneralMatchResults}>
+          <div>
+           
+            <div className={style.generalMatchesScrollContent}>
+              {soccerResults.map((result) => (
+                <div key={result.id}>
+                 <div className={style.containerMatchs}>  <img className={style.imageIcon} src={result.teamAIcon} alt="" /> <p>{result.teamAPoints}</p> <p> VS </p> <p>{result.teamBPoints}</p> <img className={style.imageIcon} src={result.teamBIcon} alt="" /></div>
+                </div>
+              ))}
+            </div>
+          </div>
+          </div>
+          <div className={style.GeneralMatchResults}>
+          <div>
+           
+            <div className={style.generalMatchesScrollContent}>
+              {basketballResults.map((result) => (
+                <div key={result.id}>
+                 <div className={style.containerMatchs}>  <img className={style.imageIcon} src={result.teamAIcon} alt="" /> <p>{result.teamAPoints}</p> <p> VS </p> <p>{result.teamBPoints}</p> <img className={style.imageIcon} src={result.teamBIcon} alt="" /></div>
+                </div>
+              ))}
+            </div>
+          </div>
+          </div>
+       
+        <div className={style.GeneralMatchResults}></div>
+        <div className={style.GeneralMatchResults}></div>
+        
     </div>
   )
 }
